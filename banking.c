d91e354c26d892def67a90ed9250fa0ca2d4338f
kavin-vsit <111116484+kavin-vsit@users.noreply.github.com>
2022-10-31 19:14:50 +0530

    Update banking.c

--- banking.c
+++ banking.c
@@ -1,30 +1,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main_menu(),balance(),deposit(),withdraw(),exit(),unavailable();
-int choice;
+void main_menu(),login(),logout(),balance(),deposit(),withdraw(),exit(),unavailable();
+int choice,login_status=0;
 
 void main_menu(){
-    printf("Check balance(1)\nDeposit money(2)\nWithdraw money(3)\nExit(0)\n");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1:
-            balance();
-            break;
-        case 2:
-            deposit();
-            break;
-        case 3:
-            withdraw();
-            break;
-        case 0:
-            printf("Exiting...\n");
-            exit(0);
-            break;
-        default:
-            printf("Invalid choice\n");
-            main_menu();
+    if(login_status==1){
+        printf("Logout(1)\nCheck balance(2)\nDeposit money(3)\nWithdraw money(4)\nExit(0)\nEnter your choice:");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                logout();
+                break;
+            case 2:
+                balance();
+                break;
+            case 3:
+                deposit();
+                break;
+            case 4:
+                withdraw();
+                break;
+            case 0:
+                printf("Exiting...\n");
+                exit(0);
+                break;
+            default:
+                printf("Invalid choice\n");
+                main_menu();
+        }
+    }else{
+        printf("Login(1)\n//Check balance(2)\n//Deposit money(3)\n//Withdraw money(4)\nExit(0)\nEnter your choice:");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                login();
+                break;
+            case 0:
+                printf("Exiting...\n");
+                exit(0);
+                break;
+            default:
+                printf("Invalid choice\n");
+                main_menu();
+        }
     }
+}
+
+void login(){
+    //do real login stuff
+    login_status=1;
+    printf("Login unsuccessful, could not contact authentication servers, logged in as demo\n");
+    main_menu();
+}
+
+void logout(){
+    char username[20] = "demo";
+    //get username
+    login_status=0;
+    printf("Logged out from %s successfully\n",username);
+    main_menu();
 }
 
 void balance(){
@@ -40,7 +75,7 @@ void withdraw(){
 }
 
 void unavailable(){
-    printf("This service is unavailable\nGo back to main menu(1)\nExit(0)\n");
+    printf("This service is unavailable\nGo back to main menu(1)\nExit(0)\nEnter your choice:");
     scanf("%d",&choice);
     switch (choice){
         case 1:
